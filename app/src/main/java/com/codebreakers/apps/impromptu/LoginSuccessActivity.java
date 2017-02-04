88dfb25bdb97587e244fc2b772c1b6bb605583d8
MohammedKhandwawala <ee16b117@ee.iitm.ac.in>
2017-02-04 12:55:54 +0530

    Merge branch 'master' of https://github.com/nithusha21/Impromptu
    # Conflicts:
    #	app/src/main/java/com/codebreakers/apps/impromptu/RegisterForm.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/codebreakers/apps/impromptu/LoginSuccessActivity.java
+++ app/src/main/java/com/codebreakers/apps/impromptu/LoginSuccessActivity.java
@@ -2,7 +2,9 @@ package com.codebreakers.apps.impromptu;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,9 +15,11 @@ public class LoginSuccessActivity extends Activity
 {
   private Button logoutButton;
 
-  public void onCreate( Bundle savedInstanceState )
+  @RequiresApi(api = Build.VERSION_CODES.ECLAIR)
+  public void onCreate(Bundle savedInstanceState )
   {
     super.onCreate( savedInstanceState );
+    overridePendingTransition(R.anim.fadein, R.anim.fadeout);
     setContentView( R.layout.login_success );
 
     initUI();
--- app/src/main/java/com/codebreakers/apps/impromptu/RegisterForm.java
+++ app/src/main/java/com/codebreakers/apps/impromptu/RegisterForm.java
@@ -1,5 +1,7 @@
 package com.codebreakers.apps.impromptu;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,10 +9,15 @@ import com.codebreakers.apps.impromptu.R;
 
 public class RegisterForm extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.ECLAIR)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            super.onCreate(savedInstanceState);
+        }
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
-        setContentView(R.layout.activity_register_form);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            setContentView(R.layout.activity_register_form);
+        }
     }
 }
