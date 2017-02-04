53b8cb0682dd262a29ae9c1d3135649e9034827c
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2017-02-04 15:48:38 +0530

    Merge branch 'master' of https://github.com/nithusha21/Impromptu
    # Conflicts:
    #	app/src/main/java/com/codebreakers/apps/impromptu/MainActivity.java

---
+++ app/src/main/java/com/codebreakers/apps/impromptu/MainControlActivity.java
@@ -0,0 +1,116 @@
+package com.codebreakers.apps.impromptu;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.backendless.Backendless;
+import com.backendless.exceptions.BackendlessFault;
+import android.content.Intent;
+
+import static com.codebreakers.apps.impromptu.R.styleable.NavigationView;
+
+public class MainControlActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_control);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_control, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            onLogoutButtonClicked();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.AddFriend) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void onLogoutButtonClicked()
+    {
+        Backendless.UserService.logout( new DefaultCallback<Void>( this )
+        {
+            @Override
+            public void handleResponse( Void response )
+            {
+                super.handleResponse( response );
+                startActivity( new Intent( MainControlActivity.this, MainActivity.class ) );
+                finish();
+            }
+
+            @Override
+            public void handleFault( BackendlessFault fault )
+            {
+                if( fault.getCode().equals( "3023" ) ) // Unable to logout: not logged in (session expired, etc.)
+                    handleResponse( null );
+                else
+                    super.handleFault( fault );
+            }
+        } );
+
+    }
+
+}
---
+++ app/src/main/java/com/codebreakers/apps/impromptu/RegisterActivity.java
@@ -0,0 +1,153 @@
+package com.codebreakers.apps.impromptu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+
+public class RegisterActivity extends Activity
+{
+  private final static java.text.SimpleDateFormat SIMPLE_DATE_FORMAT = new java.text.SimpleDateFormat( "yyyy/MM/dd" );
+
+  private EditText email;
+  private EditText nameField;
+  private EditText password;
+  private EditText phoneNumber;
+
+
+  private Button registerButton;
+
+  private String emailstring;
+  private String name;
+  private String passwordstring;
+  private String phone;
+
+
+  private BackendlessUser user;
+
+  public void onCreate( Bundle savedInstanceState )
+  {
+    super.onCreate( savedInstanceState );
+    setContentView( R.layout.activity_register_form );
+
+    initUI();
+  }
+
+  private void initUI()
+  {
+    email = (EditText) findViewById( R.id.email );
+    nameField = (EditText) findViewById( R.id.nameField );
+    password = (EditText) findViewById( R.id.password );
+    phoneNumber = (EditText)findViewById(R.id.phoneNumber);
+
+    registerButton = (Button) findViewById( R.id.registerButton );
+
+    registerButton.setOnClickListener( new View.OnClickListener()
+    {
+      @Override
+      public void onClick( View view )
+      {
+        onRegisterButtonClicked();
+      }
+    } );
+  }
+
+  private void onRegisterButtonClicked()
+  {
+    String emailText = email.getText().toString().trim();
+    String nameText = nameField.getText().toString().trim();
+    String passwordText = password.getText().toString().trim();
+    String phoneText = phoneNumber.getText().toString().trim();
+
+
+
+    if ( emailText.isEmpty() )
+    {
+      showToast( "Field 'email' cannot be empty." );
+      return;
+    }
+
+    if ( passwordText.isEmpty() )
+    {
+      showToast( "Field 'password' cannot be empty." );
+      return;
+    }
+    if ( phoneText.isEmpty() )
+    {
+      showToast( "Field 'phonenumber' cannot be empty." );
+      return;
+    }
+
+
+
+    if( !emailText.isEmpty() )
+    {
+      emailstring = emailText;
+    }
+
+    if( !nameText.isEmpty() )
+    {
+      name = nameText;
+    }
+
+    if( !phoneText.isEmpty() )
+    {
+      phone = phoneText;
+    }
+
+
+
+    if( !passwordText.isEmpty() )
+    {
+      passwordstring = passwordText;
+    }
+
+    user = new BackendlessUser();
+
+
+
+    if( email != null )
+    {
+      user.setProperty( "email", emailstring );
+    }
+
+    if( phone != null )
+    {
+      user.setProperty("phonenumber",phone);
+    }
+
+
+
+    if( name != null )
+    {
+      user.setProperty( "name", name );
+    }
+
+    if( password != null )
+    {
+      user.setProperty( "password", passwordstring );
+    }
+
+    Backendless.UserService.register( user, new DefaultCallback<BackendlessUser>( RegisterActivity.this )
+    {
+      @Override
+      public void handleResponse( BackendlessUser response )
+      {
+        super.handleResponse( response );
+        startActivity( new Intent( RegisterActivity.this, MainControlActivity.class ) );
+        finish();
+      }
+    } );
+  }
+
+  private void showToast( String msg )
+  {
+    Toast.makeText( this, msg, Toast.LENGTH_SHORT ).show();
+  }
+}
