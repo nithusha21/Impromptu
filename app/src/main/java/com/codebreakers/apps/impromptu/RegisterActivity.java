76bea414a2ccec289b85f3ae9053890291787b51
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2017-02-04 21:22:31 +0530

    Testing device ID

--- app/src/main/java/com/codebreakers/apps/impromptu/RegisterActivity.java
+++ app/src/main/java/com/codebreakers/apps/impromptu/RegisterActivity.java
@@ -146,6 +146,8 @@ public class RegisterActivity extends Activity
       user.setProperty( "password", passwordstring );
     }
 
+    user.setProperty("deviceID", Backendless.Messaging.getDeviceRegistration().getId());
+
     Backendless.UserService.register( user, new DefaultCallback<BackendlessUser>( RegisterActivity.this )
     {
       @Override
