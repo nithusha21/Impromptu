f0f0f684f01a37d37a0f9ae5d8e03e42f0d6a210
aks681 <akshayanand681@gmail.com>
2017-02-04 11:58:48 +0530

    Registration except phone number done

---
+++ app/src/main/java/com/codebreakers/apps/impromptu/Defaults.java
@@ -0,0 +1,9 @@
+package com.codebreakers.apps.impromptu;
+
+public class Defaults
+{
+  public static final String APPLICATION_ID = "48449D26-77AF-D84C-FFBC-96A6E338F700";
+  public static final String SECRET_KEY = "1F0B2C57-FE08-FE75-FFB3-57F28EBF2A00";
+  public static final String VERSION = "v1";
+  public static final String SERVER_URL = "https://api.backendless.com";
+}
