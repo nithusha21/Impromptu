45b50334acbca2aaf989fbb58d8d7b3f35e5c7b6
Nithesh <nithusha21@yahoo.co.in>
2017-02-04 16:24:44 +0530

    Random commit

--- app/src/main/java/com/codebreakers/apps/impromptu/MainControlActivity.java
+++ app/src/main/java/com/codebreakers/apps/impromptu/MainControlActivity.java
@@ -2,6 +2,7 @@ package com.codebreakers.apps.impromptu;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -73,9 +74,9 @@ public class MainControlActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Fragment fragment = null;
         if (id == R.id.AddFriend) {
-            addFriendOnClickListener();
+            fragment = new AddFriend();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -109,9 +110,6 @@ public class MainControlActivity extends AppCompatActivity
                     super.handleFault( fault );
             }
         } );
-
-    }
-    private void addFriendOnClickListener() {
 
     }
 
